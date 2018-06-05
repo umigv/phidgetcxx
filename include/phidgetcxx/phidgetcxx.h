37b45e66955a9c0227eca25d1255750f10ce88c8
Gregory Meyer <gregjm@umich.edu>
2018-06-05 17:56:28 -0400

    add ChannelClass and ChannelSubclass headers

--- include/phidgetcxx/phidgetcxx.h
+++ include/phidgetcxx/phidgetcxx.h
@@ -1,6 +1,8 @@
 #ifndef UMIGV_PHIDGETCXX_PHIDGETCXX_H
 #define UMIGV_PHIDGETCXX_PHIDGETCXX_H
 
+#include "phidgetcxx/channel_class.h"
+#include "phidgetcxx/channel_subclass.h"
 #include "phidgetcxx/exception.h"
 #include "phidgetcxx/phidget.h"
 #include "phidgetcxx/return_code.h"
