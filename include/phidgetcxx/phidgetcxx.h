f469f3c8388bb3f829aef2c09f9d1c8182ca58d4
Gregory Meyer <gregjm@umich.edu>
2018-06-05 17:55:50 -0400

    add catch-all phidgetcxx.h header

---
+++ include/phidgetcxx/phidgetcxx.h
@@ -0,0 +1,9 @@
+#ifndef UMIGV_PHIDGETCXX_PHIDGETCXX_H
+#define UMIGV_PHIDGETCXX_PHIDGETCXX_H
+
+#include "phidgetcxx/exception.h"
+#include "phidgetcxx/phidget.h"
+#include "phidgetcxx/return_code.h"
+#include "phidgetcxx/static.h"
+
+#endif
